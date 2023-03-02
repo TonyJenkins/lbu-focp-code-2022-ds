d08c55ac5db9fa1425582d050255d4d8ad88b38e
Tony Jenkins <A.M.Jenkins@leedsbeckett.ac.uk>
2023-03-02 15:07:55 +0000

    Sync with JG120

--- Week02/marks.py
+++ Week02/marks.py
@@ -6,17 +6,7 @@ if __name__ == '__main__':
 
     NUMBER_OF_MARKS = 4
 
-    marks = []
 
-    for counter in range(NUMBER_OF_MARKS):
-        while True:
-            next_mark = int(input(f'Enter mark #{counter + 1}: '))
-            if 0 <= next_mark <= 100:
-                break
-            else:
-                print('Mark out of range. Must be between 0 and 100.')
-
-        marks.append(next_mark)
 
     print()
     print(f'Highest Mark: {max(marks)}')
